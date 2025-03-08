9880fb2aedf27eb097f1f3392923673e60775ef4
Amnetik <erk1mnesia@gmail.com>
2025-03-08 02:41:53 +0100

    fix source

--- trashbot.py
+++ trashbot.py
@@ -5,13 +5,13 @@ from TrashBot.UI.key_manager import KeyListener
 trash_rocket_league: dict[str, TrashGenerator] = {
     '1' : TrashGeneratorByList('What a save', 'what_a_save.txt'),
     '2' : TrashGeneratorByList('Nice shot', 'nice_shot.txt'),
-    '3' : TrashGeneratorByList('GG', 'gg.tl'),
-    '4' : TrashGeneratorByList('GG', 'gg.tl'),
-    '5' : TrashGeneratorByList('GG', 'gg.tl'),
-    '6' : TrashGeneratorByList('GG', 'gg.tl'),
-    '7' : TrashGeneratorByList('Light insult', 'light_insult.tl'),
-    '8' : TrashGeneratorByList('Medium insult', 'medium_insult.tl'),
-    '9' : TrashGeneratorByList('Hard insult', 'gg.tl')
+    '3' : TrashGeneratorByList('GG', 'gg.txt'),
+    '4' : TrashGeneratorByList('GG', 'gg.txt'),
+    '5' : TrashGeneratorByList('GG', 'gg.txt'),
+    '6' : TrashGeneratorByList('GG', 'gg.txt'),
+    '7' : TrashGeneratorByList('Light insult', 'light_insult.txt'),
+    '8' : TrashGeneratorByList('Medium insult', 'medium_insult.txt'),
+    '9' : TrashGeneratorByList('Hard insult', 'gg.txt')
 }
 
 with KeyListener(trash_rocket_league):
